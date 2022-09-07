016d47bcd580bbf54cb9954be2a0bf67fcebd6dd
Jorge F <jorge.m.flores@protonmail.com>
2022-09-07 15:53:00 +0000

    cors for all, for now

--- packages/backEnd/server.js
+++ packages/backEnd/server.js
@@ -5,7 +5,30 @@ const port = process.env.PORT || 8080;
 require('dotenv').config()
 require('./db/associations')
 
+// Allows CORS-enabled for all http origins
+app.use(cors())
 
+// // Cors configuration
+// const whitelist = [
+//   "http://localhost:3000", "http://localhost:8080", 
+//   "http://tripplanner-client.herokuapp.com", "http://tripplanner-server.herokuapp.com", 
+//   "https://tripplanner-client.herokuapp.com", "https://tripplanner-server.herokuapp.com", 
+//   "http://wanderbranch.herokuapp.com", "http://wanderbranch-api.herokuapp.com", 
+//   "https://wanderbranch.herokuapp.com", "https://wanderbranch-api.herokuapp.com", 
+// ]
+
+// const cors_options = {
+//   origin: (origin, callback)=> {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error("Not allowed by CORS"))
+//     }
+//   },
+//   credentials: true,
+// }
+
+// app.use(cors(cors_options))
 
 
 // Parses incoming requests (eg POST body)
@@ -13,28 +36,6 @@ app.use(express.urlencoded({extended: true}))
 
 // API endpoints directory
 app.use('/', require('./api'))
-
-// Cors configuration
-const whitelist = [
-  "http://localhost:3000", "http://localhost:8080", 
-  "http://tripplanner-client.herokuapp.com", "http://tripplanner-server.herokuapp.com", 
-  "https://tripplanner-client.herokuapp.com", "https://tripplanner-server.herokuapp.com", 
-  "http://wanderbranch.herokuapp.com", "http://wanderbranch-api.herokuapp.com", 
-  "https://wanderbranch.herokuapp.com", "https://wanderbranch-api.herokuapp.com", 
-]
-
-const cors_options = {
-  origin: (origin, callback)=> {
-    if (whitelist.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-
-app.use(cors(cors_options))
 
 
 // Connect Server
